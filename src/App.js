73aa5d6801c899505a5746429e021a6c9dd54589
Medrick Zounlome <mzounlome@gmail.com>
2022-11-01 19:43:27 -0400

    adding dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+import Header from "./components/Nav/Header";
+import Input from "./components/Input/Input";
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Nav />
+      </div>
+      <div>
+        <Input />
+      </div>
     </div>
   );
 }
