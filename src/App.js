61931542efcc855adba8bc20a6b8412e49f6ebef
mzounlome <109928801+mzounlome@users.noreply.github.com>
2022-11-09 10:29:12 -0500

    Revert "making tasks"

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
 import "./App.scss";
 
-import Header from "./components/Menu/Header/Header";
+import Header from "./components/Nav/Header";
 import Input from "./components/Input/Input";
-import MainNav from "./components/Menu/Nav/Nav";
-import TaskManager from "./components/Task Menu/TaskManager";
-import { tasks } from "./data";
-import { useState } from "react";
+import Nav from "./components/Nav/Nav";
 
 function App() {
-  const [taskList, setTaskList] = useState(tasks.All);
-
-  const handleTaskList = (value) => {
-    switch (value) {
-      case "Active":
-        setTaskList(tasks.All.filter((task) => !task.completed));
-        break;
-      case "Completed":
-        setTaskList(tasks.All.filter((task) => task.completed));
-        break;
-      default:
-        setTaskList(tasks.All);
-    }
-  };
-
   return (
     <div className="App">
       <div>
         <Header />
       </div>
       <div>
-        <MainNav handleTaskList={handleTaskList} />
+        <Nav />
       </div>
       <div>
         <Input />
       </div>
-      <TaskManager tasks={taskList} />
     </div>
   );
 }
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,14 +1,7 @@
-import { SearchBar, SearchBarDiv, SearchButton } from "./Input-styles";
-
 import React from "react";
 
 const Input = () => {
-  return (
-    <SearchBarDiv>
-      <SearchBar placeholder="add details" type="search"></SearchBar>
-      <SearchButton>Add</SearchButton>
-    </SearchBarDiv>
-  );
+  return <div>Input</div>;
 };
 
 export default Input;
